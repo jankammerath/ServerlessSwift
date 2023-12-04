6f7d82da347c30d7f1f764265203f72ddb2bb1ec
Jan Kammerath <jan@lodgea.com>
2023-12-04 16:17:43 +0100

    added my forked version and cleaning to the makefile

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
+clean:
+	rm -rf .build
+	rm -f bootstrap
+	rm -f Package.resolved
+
 build:
 	docker run --rm --platform linux/arm64 -v "$(PWD):/src" -w /src swift:5.9.1-amazonlinux2 /bin/bash \
 		-c "swift build --product ServerlessSwift -c release --static-swift-stdlib -Xswiftc -static-stdlib; mv .build/release/ServerlessSwift bootstrap"
--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9.1
+// swift-tools-version: 5.9.0
 import PackageDescription
 
 let package = Package(
@@ -8,14 +8,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/vapor-community/vapor-aws-lambda-runtime", .upToNextMajor(from: "0.4.0"))
+        .package(url: "https://github.com/jankammerath/vapor-aws-lambda-runtime", branch: "main"),
     ],
     targets: [
         .executableTarget(
             name: "ServerlessSwift", 
             dependencies: [
                 .product(name: "Vapor", package: "vapor"),
-                .product(name: "VaporAWSLambdaRuntime", package: "vapor-aws-lambda-runtime")
+                .product(name: "VaporAWSLambdaRuntime", package: "vapor-aws-lambda-runtime"),
             ],
             path: ".",
             exclude: ["Makefile", "template.yaml", "sam-launch.sh", "bootstrap"]
